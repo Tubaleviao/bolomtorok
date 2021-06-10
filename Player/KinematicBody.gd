be6b4aaa3aa7d2bb2c16674507a38306fe90d5a6
Murilo de Souza Barros <murilo.barros@balera.com.br>
2021-06-10 12:52:00 -0300

    added idle sprites

--- Player/KinematicBody.gd
+++ Player/KinematicBody.gd
@@ -6,6 +6,8 @@ export var speed := 7.0
 export var gravity := 30.0
 export var jump_force := 12.0
 
+onready var idle = preload("res://Sprites/idle.png")
+
 var velocity_y := 0.0
 
 func _physics_process(delta: float) -> void:
@@ -29,14 +31,47 @@ func _unhandled_input(event: InputEvent) -> void:
 	if event.is_action_pressed("jump"):
 		velocity_y = jump_force
 #
+
+onready var sprite = $Sprite3D
+onready var animated = $Position3D/AnimatedSprite3D
+
 func _process(delta):
 	if Input.is_action_pressed("ui_down"):
+		_anim()
 		$Position3D/AnimatedSprite3D.play("front-walk")
+	elif Input.is_action_just_released("ui_down"):
+		_idle()
+		sprite.frame = 0
+		
 	elif Input.is_action_pressed("ui_left"):
+		_anim()
 		$Position3D/AnimatedSprite3D.play("left-walk")
+	elif Input.is_action_just_released("ui_left"):
+		_idle()
+		sprite.frame = 2
+		
 	elif Input.is_action_pressed("ui_right"):
+		_anim()		
 		$Position3D/AnimatedSprite3D.play("right-walk")
+	elif Input.is_action_just_released("ui_right"):
+		_idle()
+		sprite.frame = 6	
+		
 	elif Input.is_action_pressed("ui_up"):
+		_anim()
 		$Position3D/AnimatedSprite3D.play("back-walk")
+	elif Input.is_action_just_released("ui_up"):
+		_idle()
+		sprite.frame = 4
+		
 	else:
 		$Position3D/AnimatedSprite3D.stop()
+
+func _idle():
+	sprite.texture = idle
+	sprite.show()
+	animated.hide()
+	
+func _anim():
+	sprite.hide()
+	animated.show()	
